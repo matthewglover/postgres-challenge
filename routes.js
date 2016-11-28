3e6a69409d1857e1da7c4d58f6b06eb6e69d585b
Nick Field <heyimnick@live.com>
2016-11-28 13:59:32 +0000

    add file and route to create user

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 require('./init_database');
 const getUsers = require('./get_users');
+const createUser = require('./create_user');
 
 // const users = [
 //   { firstName: 'Matt', lastName: 'Glover', age: 25, type: 'worker' },
@@ -20,6 +21,21 @@ const home = {
   },
 };
 
+const create = {
+  method: 'POST',
+  path: '/createuser',
+  handler(req, reply) {
+    console.log(req.params);
+    createUser(req.params, (error, users) => {
+      if (error) console.log('Error:', error);
+      console.log(users);
+      reply.view('users', { users });
+    });
+  },
+};
+
+
 module.exports = [
   home,
+  create,
 ];
